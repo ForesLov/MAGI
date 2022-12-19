d6bbd376dffa916e0c700a07ee4cf602194f641a
IS2-19 <IS2-19@3-313B-2>
2022-12-19 09:53:56 +0300

    Добавьте файлы проекта.

---
+++ MAGI/Models/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAGI.Models
+{
+	public class Roles:EntityModel
+	{
+		public string Name { get; set; }
+	}
+}
---
+++ MAGI/Models/UsersData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAGI.Models
+{
+	public class UsersData:EntityModel
+	{
+		public string AuthenticationData { get; set; }
+		public User UserID { get; set; }
+	}
+}
