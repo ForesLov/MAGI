20ea2bed4f5f69db080d9d089cc7fe491486e686
IS2-19 <IS2-19@3-313B-2>
2023-01-16 17:23:39 +0300

    Полноценно реализован контроллер database и добавлена документация

--- MAGI/Controllers/DatabasesController.cs
+++ MAGI/Controllers/DatabasesController.cs
@@ -6,7 +6,7 @@ namespace MAGI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class DatabasesController:ControllerBase
+public class DatabasesController : ControllerBase
 {
     private readonly ApplicationDBContext _context;
 
@@ -14,60 +14,57 @@ public class DatabasesController:ControllerBase
     {
         _context = context;
     }
+    /// <summary>
+    /// Возвращает все наблюдаемые базы данных.
+    /// </summary>
+    /// <returns><see cref="IEnumerable{T}"/> где T - это наблюдаемые базы данных.</returns>
     [HttpGet]
-    public IEnumerable<Database> GetAll()
-    {
+    public ActionResult<IEnumerable<Database>> GetAll()
+    { 
         //_context.ObservedDatabases.ToArray();
-        return _context.ObservedDatabases.ToArray();
+        return Ok(_context.ObservedDatabases.ToArray());
     }
-    [HttpGet]
-    public Database? GetDatabase(Guid id)
-    {
-        return _context.ObservedDatabases.FirstOrDefault(i => i.ID == id);
-    }
+    /// <summary>
+    /// Создаёт новую запись о наблюдаемой базе данных.
+    /// </summary>
+    /// <param name="database"> 
+    /// Новый объект наблюдаемой базы данных.</param>
     [HttpPost]
-    public async Task<bool> AddDatabase([Bind("Name, ID")] Database database)
+    public async Task<IActionResult> Create([FromBody] Database database)
     {
-        try
-        {
-            _context.ObservedDatabases.Add(database);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
+        _context.ObservedDatabases.Add(database);
+        await _context.SaveChangesAsync();
+        return Ok();
     }
-    [HttpPost]
-    public async Task<bool> DeleteDatabase(Guid id)
+    /// <summary>
+    /// Возвращает наблюдаемую базу данных по идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор базы данных.</param>
+    /// <returns></returns>
+    [HttpGet("{id}")] //TODO: доделать валидацию ID
+    public ActionResult<Database?> Get(Guid id)
     {
-        try
-        {
-            var database = _context.ObservedDatabases.FirstOrDefault(i => i.ID == id);
-            if (database is null) return false;
-            _context.ObservedDatabases.Remove(database);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
+        return Ok(_context.ObservedDatabases.FirstOrDefault(i => i.ID == id));
     }
-    [HttpPost]
-    public async Task<bool> EditDatabasr([Bind ("Name, ID")]Database database)
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] Database database)
     {
-        try
-        {
-            _context.ObservedDatabases.Update(database);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
-        
+        _context.ObservedDatabases.Update(database);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var database = _context.ObservedDatabases.FirstOrDefault(i => i.ID == id);
+        if (database is null) return NotFound();
+        _context.ObservedDatabases.Remove(database);
+        await _context.SaveChangesAsync();
+        return Ok();
     }
 }
